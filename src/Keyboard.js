ef55a374068c2f19615c2c6bf6bfb6ff408685f0
Jekman <jeka_zzz@mail.ru>
2020-04-07 06:40:25 +0300

    feat: add animation to clicked button

--- src/Keyboard.js
+++ src/Keyboard.js
@@ -5,7 +5,8 @@ import buttonConfig from './buttonConfig.js';
 export default class Keyboard {
   constructor() {
     this.keyboard = null;
-    this.keyKodes = [];
+    this.textarea = null;
+    this.buttonSave = null;
   }
 
   renderKeyboard() {
@@ -16,8 +17,8 @@ export default class Keyboard {
     title.classList.add('title');
     title.textContent = 'Виртуальная клавиатура';
 
-    const textarea = document.createElement('textarea');
-    textarea.classList.add('textarea');
+    this.textarea = document.createElement('textarea');
+    this.textarea.classList.add('textarea');
 
     this.keyboard = document.createElement('div');
     this.keyboard.id = 'keyboard';
@@ -33,7 +34,7 @@ export default class Keyboard {
     helpText.classList.add('help-text');
     helpText.textContent = 'Клавиатура создана в ОС Windows. Для переключения языка используйте alt + shift. Хорошего дня! ;)';
 
-    wrapper.append(title, textarea, this.keyboard, helpText);
+    wrapper.append(title, this.textarea, this.keyboard, helpText);
 
     document.body.prepend(wrapper);
   }
@@ -52,4 +53,126 @@ export default class Keyboard {
       this.keyboard.append(row);
     }
   }
+
+  addListeners() {
+    document.addEventListener('keydown', this.keydownHandler.bind(this));
+    document.addEventListener('keyup', this.keyupHandler.bind(this));
+
+    this.keyboard.addEventListener('mousedown', this.mousedownHandler.bind(this));
+    this.keyboard.addEventListener('mouseup', this.mouseupHandler.bind(this));
+  }
+
+  keydownHandler(event) {
+    event.preventDefault();
+
+    const buttonCode = event.code;
+    this.addAnimateToButton(buttonCode);
+
+    const buttonKey = event.key;
+    this.findButton(buttonKey);
+  }
+
+  keyupHandler(event) {
+    const buttonCode = event.code;
+
+    this.removeAnimateFromButton(buttonCode);
+  }
+
+  mousedownHandler(event) {
+    const button = event.target.closest('.button');
+
+    if (button) {
+      button.classList.add('animated');
+      button.addEventListener('mouseleave', this.mouseleaveHandler, {once: true});
+
+      const buttonCode = button.classList[1];
+      console.log(buttonCode);
+
+      //this.findButton(buttonKey);
+    }
+  }
+
+  mouseleaveHandler(event) {
+    const button = event.target;
+    button.classList.remove('animated');
+  }
+
+  mouseupHandler(event) {
+    const button = event.target.closest('.button');
+
+    if (button) {
+      button.classList.remove('animated');
+    }
+  }
+
+  addAnimateToButton(code) {
+    const button = this.keyboard.querySelector(`.${code}`);
+
+    if (button) {
+      button.classList.add('animated');
+    }
+  }
+
+  removeAnimateFromButton(code) {
+    const button = this.keyboard.querySelector(`.${code}`);
+
+    if (button) {
+      button.classList.remove('animated');
+    }
+  }
+
+  findButton(buttonKey) {
+    switch (buttonKey) {
+      case 'Tab':
+        console.log('Tab');
+        break;
+
+      case 'CapsLock':
+
+        break;
+
+      case 'Shift':
+
+        break;
+
+      case 'Control':
+
+        break;
+
+      case 'Meta':
+
+        break;
+
+      case 'Alt':
+
+        break;
+      case 'Backspace':
+
+        break;
+      case 'Delete':
+
+        break;
+      case 'Enter':
+
+        break;
+      case 'ArrowLeft':
+
+        break;
+      case 'ArrowDown':
+
+        break;
+      case 'ArrowRight':
+
+        break;
+      case 'ArrowUp':
+
+        break;
+
+      default:
+        this.textarea.value += buttonKey;
+
+        break;
+    }
+  }
+
 }
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import Keyboard from './Keyboard.js';
 function initialize() {
   const keyboard = new Keyboard();
   keyboard.renderKeyboard();
+  keyboard.addListeners();
 }
 
 document.addEventListener('DOMContentLoaded', initialize);
